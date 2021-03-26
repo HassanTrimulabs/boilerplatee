e8d1dd8c0710cc7f8948c629ec23d155962f5977
HassanTrimulabs <hassan@trimulabs.com>
2021-03-26 17:53:29 +0500

    router & routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, Router } from "react-router";
+import { createBrowserHistory } from "history";
+import "./App.css";
+import ROUTES from "./routes";
+
+const history = createBrowserHistory();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Switch>
+          {ROUTES.map((route) => {
+            const { component, exact, path } = route;
+
+            return (
+              <Route
+                path={path}
+                exact={exact}
+                key={path}
+                component={component}
+              />
+            );
+          })}
+        </Switch>
+      </Router>
     </div>
   );
 }
